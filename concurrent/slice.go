d30a244e0510028b01c2f9e97f7b6952c1a00a82
Evert Joubert <evocert@gmail.com>
2024-11-18 09:02:20 +0200

    add func (slce *Slice) Indexes(index ...int) (idxs []int)

--- concurrent/slice.go
+++ concurrent/slice.go
@@ -136,6 +136,27 @@ func (slce *Slice) Iterate(index ...int) func(func(int, interface{}) bool) {
 	}
 }
 
+func (slce *Slice) Indexes(index ...int) (idxs []int) {
+	il := len(index)
+	if slce != nil {
+		if mp := slce.Map; mp != nil {
+			sort.Slice(index, func(i, j int) bool { return index[i] < index[j] })
+			mp.Range(func(k interface{}, v interface{}) (stop bool) {
+				if il > 0 {
+					if index[0] == k {
+						idxs = append(idxs, k.(int))
+						index = index[1:]
+						il--
+					}
+					return !(il > 0)
+				}
+				idxs = append(idxs, k.(int))
+				return true
+			})
+		}
+	}
+	return
+}
 func (slce *Slice) Values(index ...int) (vals []interface{}) {
 	if il := len(index); slce != nil && il > 0 {
 		if mp := slce.Map; mp != nil {
