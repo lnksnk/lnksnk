20f9e5acaca33e1f7026836aa7ef475ad5aca932
Evert Joubert <evocert@gmail.com>
2025-04-18 18:03:40 +0200

    func Fonts(paths ...string) fsembed.EmbedFS to help in creating a universal way to indicate what to import (embed) from roboto and material font packages

--- fonts/fonts.go
+++ fonts/fonts.go
@@ -1,13 +1,39 @@
 package fonts
 
 import (
-	_ "github.com/lnksnk/lnksnk/fonts/material"
-	_ "github.com/lnksnk/lnksnk/fonts/robotov27latin"
-	"github.com/lnksnk/lnksnk/resources"
+	"embed"
+	"io/fs"
+
+	fsembed "github.com/lnksnk/lnksnk/fs/embed"
 )
 
-func init() {
-	gblrsfs := resources.GLOBALRSNG().FS()
-	gblrsfs.MKDIR("/fonts")
-	gblrsfs.SET("/fonts/head.html", gblrsfs.CAT("/fonts/material/head.html"), "\r\n", gblrsfs.CAT("/fonts/roboto/head.html"))
+type FSFonts struct {
+	fsembed.EmbedFSOpen
+	fsembed.EmbedFSReadDir
+	fsembed.EmbedFSReadFile
+}
+
+//go:embed *
+var FontsFS embed.FS
+
+func Fonts(paths ...string) fsembed.EmbedFS {
+	if paths == nil {
+		paths = append(paths, "roboto", "material")
+	}
+	return &struct {
+		fsembed.EmbedFSOpen
+		fsembed.EmbedFSReadDir
+		fsembed.EmbedFSReadFile
+	}{EmbedFSOpen: fsembed.EmbedFSOpenFunc(func(name string) (fs.File, error) {
+		return FontsFS.Open(name)
+	}), EmbedFSReadDir: fsembed.EmbedFSReadDirFunc(func(name string) ([]fs.DirEntry, error) {
+		for _, pth := range paths {
+			if pthl, nml := len(pth), len(name); pthl <= nml && name[:pthl] == pth {
+				return FontsFS.ReadDir(name)
+			}
+		}
+		return FontsFS.ReadDir(name)
+	}), EmbedFSReadFile: fsembed.EmbedFSReadFileFunc(func(name string) ([]byte, error) {
+		return FontsFS.ReadFile(name)
+	})}
 }
