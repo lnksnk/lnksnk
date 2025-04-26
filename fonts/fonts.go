d417dfa3b0115755fb912ef82cb3defb5cd3b816
Evert Joubert <evocert@gmail.com>
2025-04-26 07:25:29 +0200

    remove FSFonts struct, rename EmbdedFonts to ImportFonts and reference fs.ImortResources

--- fonts/fonts.go
+++ fonts/fonts.go
@@ -4,21 +4,20 @@ import (
 	"embed"
 
 	fs "github.com/lnksnk/lnksnk/fs"
-	fsembed "github.com/lnksnk/lnksnk/fs/embed"
 	"github.com/lnksnk/lnksnk/ioext"
 )
 
 type FSFonts struct {
-	fsembed.EmbedFSOpen
-	fsembed.EmbedFSReadDir
-	fsembed.EmbedFSReadFile
+	fs.IOFSOpen
+	fs.IOFSReadDir
+	fs.IOFSReadFile
 }
 
 //go:embed *
 var FontsFS embed.FS
 
-func EmbedFonts(fsys fs.MultiFileSystem) {
-	fsembed.ImportResource(func(srcroot string, src *ioext.Buffer, srcfsys fs.MultiFileSystem) {
+func ImportFonts(fsys fs.MultiFileSystem) {
+	fs.ImportResource(func(srcroot string, src *ioext.Buffer, srcfsys fs.MultiFileSystem) {
 		srcfsys.Map(srcroot)
 		srcfsys.Set(srcroot+"/index.html", src)
 	}, fsys, FontsFS, ".css", ".go", true, "/fonts", "material", "roboto")
