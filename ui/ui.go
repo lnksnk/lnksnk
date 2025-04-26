6cb68090cae579d1f93a4ce1b345d8a506699d9c
Evert Joubert <evocert@gmail.com>
2025-04-26 07:26:16 +0200

    rename EmbedUiJS to ImportUiJS and reference fs.ImortResources

--- ui/ui.go
+++ ui/ui.go
@@ -4,15 +4,15 @@ import (
 	"embed"
 
 	"github.com/lnksnk/lnksnk/fs"
-	fsembed "github.com/lnksnk/lnksnk/fs/embed"
+
 	"github.com/lnksnk/lnksnk/ioext"
 )
 
 //go:embed js/*.*
 var UiJsFS embed.FS
 
-func EmbedUiJS(fsys fs.MultiFileSystem) {
-	fsembed.ImportResource(func(srcroot string, src *ioext.Buffer, srcfsys fs.MultiFileSystem) {
+func ImportUiJS(fsys fs.MultiFileSystem) {
+	fs.ImportResource(func(srcroot string, src *ioext.Buffer, srcfsys fs.MultiFileSystem) {
 		srcfsys.Map(srcroot)
 		srcfsys.Set(srcroot+"/index.html", src)
 	}, fsys, UiJsFS, ".js", ".go", true, "/ui", "js")
