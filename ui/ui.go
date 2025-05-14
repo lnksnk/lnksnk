50a49d2c5ea586a97f44f709cbb81631615cf2f5
Evert Joubert <evocert@gmail.com>
2025-05-14 09:48:00 +0200

    add a waay to specify alternate root for source referenced index.html file only if the index.html file can not be created in the original srcroot

--- ui/ui.go
+++ ui/ui.go
@@ -11,9 +11,19 @@ import (
 //go:embed js/*.*
 var UiJsFS embed.FS
 
-func ImportUiJS(fsys fs.MultiFileSystem) {
+func ImportUiJS(fsys fs.MultiFileSystem, altroot ...string) {
 	fs.ImportResource(func(srcroot string, src *ioext.Buffer, srcfsys fs.MultiFileSystem) {
 		srcfsys.Map(srcroot)
-		srcfsys.Set(srcroot+"/index.html", src)
+		if !srcfsys.Exist(srcroot + "/index.html") {
+			srcfsys.Set(srcroot+"/index.html", src)
+		} else if len(altroot) > 0 && altroot[0] != "" && altroot[0] != "/" && altroot[0][0] == '/' {
+			if altroot[0][len(altroot[0])-1] == '/' {
+				altroot[0] = altroot[0][:len(altroot[0])-1]
+			}
+			if altroot[0] != srcroot {
+				srcfsys.Map(altroot[0])
+				srcfsys.Set(altroot[0]+"/index.html", src)
+			}
+		}
 	}, fsys, UiJsFS, ".js", ".go", true, "/ui", "js")
 }
