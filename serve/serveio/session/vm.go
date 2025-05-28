9528283982df2fc6f56f648e965cd1b84aebd84d
Evert Joubert <evocert@gmail.com>
2025-05-28 21:45:58 +0200

    extend SessionVm API  with     SetWriter(io.Writer) 	Writer() io.Writer 	Print(...interface{}) error 	Println(...interface{}) error

--- serve/serveio/session/vm.go
+++ serve/serveio/session/vm.go
@@ -1,14 +1,88 @@
 package sessioning
 
+import (
+	"io"
+
+	"github.com/lnksnk/lnksnk/ioext"
+	"github.com/lnksnk/lnksnk/serve/serveio"
+)
+
 type SessionVM interface {
 	VM() interface{}
 	Session() Session
 	Set(string, interface{}) error
+	SetWriter(io.Writer)
+	Writer() io.Writer
+	Print(...interface{}) error
+	Println(...interface{}) error
 }
 
 type sessionvm struct {
 	orgvm interface{}
 	ssn   Session
+	wrtrs []io.Writer
+}
+
+// Writer implements SessionVM.
+func (s *sessionvm) Writer() io.Writer {
+	if s == nil {
+		return nil
+	}
+	if wtrsl := len(s.wrtrs); wtrsl > 0 {
+		return s.wrtrs[wtrsl-1]
+	}
+	return nil
+}
+
+// SetWriter implements SessionVM.
+func (s *sessionvm) SetWriter(wrtr io.Writer) {
+	if s != nil {
+		if wrtr != nil {
+			s.wrtrs = append(s.wrtrs, wrtr)
+			return
+		}
+		wtrsl := len(s.wrtrs)
+		if wtrsl > 0 {
+			if wtrsl == 1 {
+				s.wrtrs = nil
+				return
+			}
+			s.wrtrs = s.wrtrs[:wtrsl-1]
+			return
+		}
+	}
+}
+
+// Print implements SessionVM.
+func (s *sessionvm) Print(a ...interface{}) (err error) {
+	if s == nil || len(a) == 0 {
+		return
+	}
+	if wtrsl := len(s.wrtrs); wtrsl > 0 {
+		if wrtr := s.wrtrs[wtrsl-1]; wrtr != nil {
+			if srvwtr, _ := wrtr.(serveio.Writer); srvwtr != nil {
+				return srvwtr.Print(a...)
+			}
+			err = ioext.Fprint(wrtr, a...)
+		}
+	}
+	return
+}
+
+// Println implements SessionVM.
+func (s *sessionvm) Println(a ...interface{}) (err error) {
+	if s == nil || len(a) == 0 {
+		return
+	}
+	if wtrsl := len(s.wrtrs); wtrsl > 0 {
+		if wrtr := s.wrtrs[wtrsl-1]; wrtr != nil {
+			if srvwtr, _ := wrtr.(serveio.Writer); srvwtr != nil {
+				return srvwtr.Println(a...)
+			}
+			err = ioext.Fprintln(wrtr, a...)
+		}
+	}
+	return
 }
 
 // VM implements SessionVM.
