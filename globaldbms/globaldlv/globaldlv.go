0b7119f7505ac0efbabb653caf4fc5c844395fc6
Evert Joubert <evocert@gmail.com>
2025-07-13 07:34:46 +0200

    miner code change

--- globaldbms/globaldlv/globaldlv.go
+++ globaldbms/globaldlv/globaldlv.go
@@ -3,11 +3,9 @@ package globaldlv
 import (
 	"github.com/lnksnk/lnksnk/dbms/dlv"
 	"github.com/lnksnk/lnksnk/globaldbms"
-	"github.com/lnksnk/lnksnk/globalfs"
 )
 
 func init() {
 	globaldbms.MapDriver("csv", dlv.InvokeCSVDB, dlv.ParseSqlParam)
 	globaldbms.MapDriver("dlv", dlv.InvokeDLVDB, dlv.ParseSqlParam)
-	globaldbms.DRIVERS.Register("csv", globalfs.GLOBALFS)
 }
